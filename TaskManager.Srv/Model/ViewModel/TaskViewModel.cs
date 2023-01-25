cb907449b0d5425238bfed115c91cc3417a691ec
Melkvi Dániel <melkvi.daniel@griffsoft.hu>
2023-01-25 11:14:41 +0100

    task viewmodel

---
+++ TaskManager.Srv/Model/ViewModel/TaskViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Srv.Model.ViewModel;
+
+public class TaskViewModel
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string State { get; set; } = "";
+}
