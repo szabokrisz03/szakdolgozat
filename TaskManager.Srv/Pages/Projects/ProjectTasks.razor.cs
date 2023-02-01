eac40dae17742d59b052ef4eacd30d8b70c95e9d
Szabó Krisztián <KrisztianDavid.Szabo@griffsoft.hu>
2023-02-01 14:23:55 +0100

    #64638 - refactor

--- TaskManager.Srv/Pages/Projects/ProjectTasks.razor.cs
+++ TaskManager.Srv/Pages/Projects/ProjectTasks.razor.cs
@@ -8,6 +8,8 @@ namespace TaskManager.Srv.Pages.Projects;
 
 public partial class ProjectTasks
 {
+    private MudTable<TaskViewModel> _table;
+    private long ShownId = 0;
     [Parameter] public string TechnicalName { get; set; } = "";
 
     private async Task<TableData<TaskViewModel>> LoadData(TableState state)
@@ -16,6 +18,17 @@ public partial class ProjectTasks
         int take = state.PageSize;
 
         var tasks = new List<TaskViewModel>();
+        for(int i = 0; i < 12; i++)
+        {
+            tasks.Add(new TaskViewModel
+            {
+                RowId = i + 1,
+                Name = "teszt1",
+                State = "folyamatban",
+                Description = "alma alma alma alma alma alma alma alma alma alma alma alma alma alma alma alma alma alma "
+            }); 
+        }
+
         var size = tasks.Count();
 
         return new TableData<TaskViewModel>
@@ -23,5 +36,23 @@ public partial class ProjectTasks
             Items = tasks.Skip(skip).Take(take),
             TotalItems = size
         };
+    }
+
+    private async Task PageChanged(int i)
+    {
+        _table.NavigateTo(i - 1);
+        
+    }
+
+    private void ShowBtnPress(TaskViewModel taskView)
+    {
+        if (ShownId == taskView.RowId)
+        {
+            ShownId = 0;
+        }
+        else
+        {
+            ShownId = taskView.RowId;
+        }
     }
 }
