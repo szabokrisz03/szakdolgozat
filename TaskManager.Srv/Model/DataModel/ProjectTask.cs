fb4a23cfc10c14f7f52791b0397b0ea92e1d4a38
Melkvi Dániel <daniel.melkvi@griffsoft.hu>
2023-02-21 09:57:16 +0000

    Updated ProjectTask.cs

--- TaskManager.Srv/Model/DataModel/ProjectTask.cs
+++ TaskManager.Srv/Model/DataModel/ProjectTask.cs
@@ -5,6 +5,7 @@ public class ProjectTask: DbTable
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
     public Guid TechnicalName { get; set; }
+    public TaskState State { get; set; }
 
     public ICollection<TaskMilestone> Milestones { get; set; }
 }
