53092f654e955d82fa12b8e380276b2e646b4b80
Melkvi Dániel <daniel.melkvi@griffsoft.hu>
2023-02-21 10:35:21 +0000

    Updated ProjectTask.cs

--- TaskManager.Srv/Model/DataModel/ProjectTask.cs
+++ TaskManager.Srv/Model/DataModel/ProjectTask.cs
@@ -2,6 +2,7 @@
 
 public class ProjectTask: DbTable
 {
+    public int ProjectId { get; set; }
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
     public Guid TechnicalName { get; set; }
