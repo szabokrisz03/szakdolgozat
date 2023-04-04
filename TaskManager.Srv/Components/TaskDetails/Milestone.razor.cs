93bc4bed9976753800a2aa8ddecd114ac222c5d8
Szabó Krisztián <KrisztianDavid.Szabo@griffsoft.hu>
2023-04-04 11:05:34 +0200

    #66157 - Milestones

--- TaskManager.Srv/Components/TaskDetails/Milestone.razor.cs
+++ TaskManager.Srv/Components/TaskDetails/Milestone.razor.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
 
+using MudBlazor;
+using MudBlazor.Utilities;
+
+using TaskManager.Srv.Model.ViewModel;
+
 namespace TaskManager.Srv.Components.TaskDetails;
 
 public partial class Milestone
@@ -7,8 +12,22 @@ public partial class Milestone
     [Parameter]
     public bool IsOpen { get; set; }
 
+    private List<MilestoneViewModel> MilestoneList = new()
+    {
+        new MilestoneViewModel() { Id="main", Date= DateTime.Now, Name="1. name"},
+        new MilestoneViewModel() { Id="main", Date= DateTime.Now, Name="2. name"},
+        new MilestoneViewModel() { Id="main", Date= DateTime.Now, Name="3. name"},
+        new MilestoneViewModel() { Id="main", Date= DateTime.Now, Name="4. name"},
+        new MilestoneViewModel() { Id="main", Date= DateTime.Now, Name="5. name"},
+    };
+      
+    private void Update(MudItemDropInfo<MilestoneViewModel> info)
+    {
+        info.Item.Id = info.DropzoneIdentifier;
+    }
+
     private void ToggleDrawer()
     {
-       IsOpen= !IsOpen;
+        IsOpen = !IsOpen;
     }
 }
---
+++ TaskManager.Srv/Model/ViewModel/MilestoneViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Srv.Model.ViewModel;
+
+public class MilestoneViewModel
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+}
