0f39c666be6deae9874e1b9f249546d952e1f29d
Szabó Krisztián <KrisztianDavid.Szabo@griffsoft.hu>
2023-09-05 11:08:38 +0200

    #70209 - Filter p1

---
+++ TaskManager.Srv/Components/TaskDetails/Filter.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using TaskManager.Srv.Model.ViewModel;
+
+namespace TaskManager.Srv.Components.TaskDetails;
+
+public partial class Filter
+{
+
+    private bool isOpen = false;
+    [Parameter] public StateFilterViewModell? StateFilterViewModell { get; set; }
+
+    protected override void OnInitialized()
+    {
+        StateFilterViewModell = new StateFilterViewModell();
+    }
+
+    private void FilterOpenChanged()
+    {
+        isOpen = !isOpen;
+    }
+}
---
+++ TaskManager.Srv/Model/ViewModel/StateFilterViewModell.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Srv.Model.ViewModel;
+
+public class StateFilterViewModell
+{
+    [Display(Name = "Igényfelmérés")]
+    public bool Igenyfelmeres { get; set; } = true;
+    [Display(Name = "Specifikáció alatt")]
+    public bool SpecifikacioAlatt { get; set; } = true;
+    [Display(Name = "Fejlesztésre vár")]
+    public bool FejlsztesreVar { get; set; } = true;
+    [Display(Name = "Fejlesztés alatt")]
+    public bool FejlesztesAlatt { get; set; } = true;
+    [Display(Name = "Tesztelés alatt")]
+    public bool TesztelesAlatt { get; set; } = true;
+    [Display(Name = "Kiadásra vár")]
+    public bool KiadasraVar { get; set; } = true;
+    [Display(Name = "Verziózva")]
+    public bool Verziozva { get; set; } = true;
+    [Display(Name = "Ajánlatadás")]
+    public bool Ajanlatadas { get; set; } = true;
+    [Display(Name = "Meghiúsult")]
+    public bool Meghiusult { get; set; } = true;
+}
